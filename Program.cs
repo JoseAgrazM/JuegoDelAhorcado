611f9de45480e58e2296a6e175b973df7fa6233b
Jose Agraz <joseagrazm@gmail.com>
2023-10-02 19:56:57 +0200

    by joseAgraz

--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
-﻿namespace Ahorcado_Juego
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Ahorcado_Juego
 {
     internal class Program
     {
@@ -8,14 +12,26 @@
         private static int intentosMaximos = 6;
         private static int intentosFallidos = 0;
         private static bool partidaEnJuego = true;
-        private static string palabra = palabraAleatoria();
-        private static string palabraEnBlanco = new string('↑', palabra.Length);
+        private static string palabra = "";
+        private static string palabraEnBlanco = "";
+        private static HashSet<string> palabrasUsadas = new HashSet<string>();
+        private static Stack<char> letrasErroneas = new Stack<char>();
+        
 
-        // ----------------- FUNCTION MAIN -------------------------------------------------------
-        static void Main(string[] args)
+
+    // ----------------- FUNCTION MAIN -------------------------------------------------------
+    static void Main(string[] args)
         {
+            
+            palabra = palabraAleatoria();
+            palabraEnBlanco = new string('↑', palabra.Length);
+
             empezarJuego();
             logicaDelJuego();
+            mostrarLetrasErroneas();
+
+
+
             Console.ReadLine();
         }
 
@@ -37,8 +53,9 @@
 
         static void empezarJuego()
         {
+
             Console.WriteLine("¡Bienvenido al juego del ahorcado!");
-            Console.WriteLine("----------------------------------");
+            Console.WriteLine("---------- By: JoseAgraz ---------");
             Console.WriteLine(tableroVisual(tablero));
             Console.WriteLine(palabraEnBlanco);
             Console.WriteLine();
@@ -48,13 +65,42 @@
         static string palabraAleatoria()
         {
             string[] palabras = new string[]
-            { "perro", "gato", "coche", "casa"};
+            {
+                "Manzana", "Banana", "Perro", "Gato", "Casa", "Coche", "Pelota", "Arbol", "Sol", "Luna",
+                "Libro", "Computadora", "Café", "Pizza", "Playa", "Rio", "Guitarra", "Pintura", "Telefono", "Amigo",
+                "Familia", "Trabajo", "Escuela", "Deporte", "Viaje", "Musica", "Cine", "Ciencia", "Aventura", "Verano",
+                "Invierno", "Primavera", "Otoño", "Amor", "Paz", "Felicidad", "Tiempo", "Internet", "Fiesta", "Dinero",
+                "Naturaleza", "Arte", "Comida", "Historia", "Religion", "Politica", "Tecnologia", "Animales", "Flor",
+                "Camino", "Viento", "Cielo", "Aire", "Almohada", "Cepillo", "Botella", "Silla", "Zapato", "Bolsa",
+                "Jardin", "Juego", "Hermano", "Hermana", "Profesor", "Estudiante", "Medicina", "Rey", "Reina",
+                "Cuento", "Carta", "Regalo", "Piedra", "Raton", "Papel", "Reloj", "Tijeras", "Lapiz", "Rosa",
+                "Tigre", "Leon", "Elefante", "Tortuga", "Pajaro", "Mariposa", "Serpiente", "Rana", "Pez",
+                "Escorpion", "Dragon", "Hormiga", "Mosquito", "Estrella", "Planeta", "Galaxia", "Universo",
+                "Circulo", "Cuadrado", "Triangulo", "Rectangulo", "Piramide", "Esfera", "Cubo", "Cono",
+                "Madera", "Metal", "Plastico", "Vidrio" 
+            };
 
             Random random = new Random();
 
-            int aleatorio = random.Next(0, palabras.Length);
+            string palabraFiltrada; 
 
-            return palabras[aleatorio].ToUpper();
+            do
+            {
+                int aleatorio = random.Next(0, palabras.Length);
+                palabraFiltrada = palabras[aleatorio].ToUpper();
+
+                palabraFiltrada = palabraFiltrada.Replace("Á", "A");
+                palabraFiltrada = palabraFiltrada.Replace("É", "E");
+                palabraFiltrada = palabraFiltrada.Replace("Í", "I");
+                palabraFiltrada = palabraFiltrada.Replace("Ó", "O");
+                palabraFiltrada = palabraFiltrada.Replace("Ú", "U");
+
+            } while (palabrasUsadas.Contains(palabraFiltrada));
+
+            palabrasUsadas.Add(palabraFiltrada); // Agregar la palabra a la lista de palabras usadas
+
+            return palabraFiltrada;
+
         }
 
         // ---------------- PONER LETRA -------------------------------------------------------------
@@ -94,17 +140,144 @@
         {
             char[] palabraEnBlancoArray = palabraEnBlanco.ToCharArray();
 
+            bool letraEncontrada = false;
+
             for (int i = 0; i < palabra.Length; i++)
             {
 
                 if (palabra[i] == letra)
                 {
                     palabraEnBlancoArray[i] = letra;
+                    letraEncontrada = true;
                 }
             }
+
+            if (!letraEncontrada)
+            {
+                letrasErroneas.Push(letra);
+            }
+
+            
             return new string (palabraEnBlancoArray);
         }
 
+        // --------------- LETRAS ERRONEAS ---------------------------------------------------------
+
+        static void mostrarLetrasErroneas()
+        {
+            if (letrasErroneas.Count >= 0)
+            {
+                var letrasErroneasComoString = new List<string>();
+                foreach (char letra in letrasErroneas)
+                {
+                    letrasErroneasComoString.Add(letra.ToString());
+                }
+
+                Console.WriteLine("Letras incorrectas: " + string.Join(", ", letrasErroneasComoString));
+            }
+            else
+            {
+                Console.WriteLine("");
+            }
+        }
+
+
+        //------------------ REINICIAR JUEGO-------------------------------------------------------
+
+        static void ReiniciarJuego()
+        {
+            Console.WriteLine("Presiona 'R' para restablecer o cualquier otra tecla para salir.");
+            char respuesta = Console.ReadKey().KeyChar;
+            if (respuesta == 'R' || respuesta == 'r')
+            {
+                // Reinicia las variables y el juego
+                Console.Clear();
+                intentosFallidos = 0;
+                palabra = palabraAleatoria();
+                palabraEnBlanco = new string('↑', palabra.Length);
+
+                partidaEnJuego = true;
+                empezarJuego();
+            }
+            else
+            {
+                partidaEnJuego = false;
+            }
+        }
+
+        // ----------------- JUEGO EN CURSO ---------------------------------------------------------
+        static void juegoEnCurso()
+        {
+
+            Console.WriteLine("¡Bienvenido al juego del ahorcado!");
+            Console.WriteLine("---------- By: JoseAgraz ---------");
+            Console.WriteLine();
+
+            mostrarLetrasErroneas();
+
+            Console.WriteLine($"Intentos fallidos {intentosFallidos}/{intentosMaximos}");
+            Console.WriteLine(tableroVisual(tablero, intentosFallidos));
+            Console.WriteLine();
+            Console.WriteLine(palabraEnBlanco);
+        }
+
+        // ----------------- LOGICA DEL JUEGO ---------------------------------------------------------
+
+        static void logicaDelJuego()
+        {
+            while (partidaEnJuego)
+            {
+                
+                char letra = ponerLetra(palabraEnBlanco);
+                Console.WriteLine();
+
+                if (palabra.Contains(letra.ToString()))
+                {
+                    palabraEnBlanco = verificarLetra(palabra, letra, palabraEnBlanco);
+                    Console.Clear();
+
+                    juegoEnCurso();
+
+
+                    Console.WriteLine();
+
+                    if (!palabraEnBlanco.Contains('↑'))
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine("¡GANASTE!");
+                        Console.WriteLine();
+                        partidaEnJuego = false;
+                        ReiniciarJuego();
+                    }
+                }
+                else
+                {
+                    intentosFallidos++;
+                    Console.Clear();
+                    juegoEnCurso();
+
+
+                    Console.WriteLine();
+
+
+                    if (intentosFallidos >= intentosMaximos)
+        
+                    {
+                        Console.WriteLine("Perdiste!");
+                        Console.WriteLine();
+                        Console.WriteLine($"La palabra correcta era: {palabra}");
+                        partidaEnJuego = false;
+                        ReiniciarJuego();
+
+                    }
+                    
+
+                    
+                }
+
+            }
+
+        }
 
         // --------------- ETAPAS DEL JUEGO ---------------------------------------------------------
         static string tableroVisual(char[,] tablero, int intentosFallidos)
@@ -164,98 +337,5 @@
             return etapasHorca[Math.Min(intentosFallidos, etapasHorca.Length - 1)];
         }
 
-        //------------------ REINICIAR JUEGO-------------------------------------------------------
-
-        static void ReiniciarJuego()
-        {
-            Console.WriteLine("Presiona 'R' para restablecer o cualquier otra tecla para salir.");
-            char respuesta = Console.ReadKey().KeyChar;
-            if (respuesta == 'R' || respuesta == 'r')
-            {
-                // Reinicia las variables y el juego
-                Console.Clear();
-                intentosFallidos = 0;
-                palabra = palabraAleatoria();
-                palabraEnBlanco = new string('↑', palabra.Length);
-
-                partidaEnJuego = true;
-                empezarJuego();
-            }
-            else
-            {
-                partidaEnJuego = false;
-            }
-        }
-
-        // ----------------- JUEGO EN CURSO ---------------------------------------------------------
-        static void juegoEnCurso()
-        {
-            Console.WriteLine("¡Bienvenido al juego del ahorcado!");
-            Console.WriteLine("----------------------------------");
-            Console.WriteLine($"Intentos fallidos {intentosFallidos}/{intentosMaximos}");
-            Console.WriteLine(tableroVisual(tablero, intentosFallidos));
-            Console.WriteLine();
-            Console.WriteLine(palabraEnBlanco);
-        }
-
-        // ----------------- LOGICA DEL JUEGO ---------------------------------------------------------
-
-        static void logicaDelJuego()
-        {
-            while (partidaEnJuego)
-            {
-                string letrasErroneas;
-
-                char letra = ponerLetra(palabraEnBlanco);
-                Console.WriteLine();
-
-                if (palabra.Contains(letra.ToString()))
-                {
-                    palabraEnBlanco = verificarLetra(palabra, letra, palabraEnBlanco);
-                    Console.Clear();
-
-                    juegoEnCurso();
-
-                    Console.WriteLine();
-
-                    if (!palabraEnBlanco.Contains('↑'))
-                    {
-                        Console.WriteLine();
-                        Console.WriteLine("¡GANASTE!");
-                        Console.WriteLine();
-                        partidaEnJuego = false;
-                        ReiniciarJuego();
-
-                        
-                    }
-                }
-                else
-                {
-                    intentosFallidos++;
-                    Console.Clear();
-
-                    juegoEnCurso();
-
-                    Console.WriteLine();
-
-
-                    if (intentosFallidos >= intentosMaximos)
-                    {
-                        Console.WriteLine("Perdiste!");
-                        Console.WriteLine();
-                        Console.WriteLine($"La palabra correcta era: {palabra}");
-                        partidaEnJuego = false;
-                        ReiniciarJuego();
-
-                    }
-
-                }
-
-            }
-
-        }
-
-
-        
     }
 }
